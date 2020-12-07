3577b2c4260991d1bd7c1a1f4f2f7511f0b17481
Maria Zavala <pazrodriguez612@gmail.com>
2020-12-07 17:46:32 -0600

    Making window

--- src/com/pazzy/Game.java
+++ src/com/pazzy/Game.java
@@ -1,16 +1,30 @@
 package com.pazzy;
 
-public class Game implements Runnable{
-    // Screen resolution
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable{
+    private static final long serialVersionUID = 1L;
+        // Screen resolution
     public static int width = 300;
     public static int height = width / 16  * 9;
     public static int scale = 3;    // For better performance
 
-    // Subprocess
+        // Subprocess
     private Thread thread;
+    private JFrame frame;
     private boolean running = false;
 
-    // synchronized makes sure theres no thread interferences and memory consistency errors.
+        // Constructor First thing that will happen
+    public Game() {
+        Dimension size = new Dimension(width * scale, height * scale);
+        setPreferredSize(size);
+
+        frame = new JFrame();
+    }
+
+        // start method - synchronized makes sure theres no
+        // thread interferences and memory consistency errors.
     public synchronized void start() {
         running = true;
         // Creating new thread object, "this makes sure it is including the "Game" class
@@ -19,7 +33,7 @@ public class Game implements Runnable{
         thread.start();
     }
 
-    // Stopping the thread
+        // stop method - Stopping the thread
     public synchronized void stop() {
         running = false;
         try {
@@ -29,11 +43,24 @@ public class Game implements Runnable{
         }
     }
 
-    // Game loop
+        // run method - Game loop
     public void run() {
         while (running) {
-
+            System.out.println("Hello bitchess.....");
         }
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.frame.setResizable(false);
+        game.frame.setTitle("Rain");
+        game.frame.add(game);
+        game.frame.pack();
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.frame.setLocationRelativeTo(null);
+        game.frame.setVisible(true);
+
+        game.start();
     }
 
 }
